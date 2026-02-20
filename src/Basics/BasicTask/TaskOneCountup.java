0d3279a3ccc2b31a98c3f201712e84b3df57192e
Saivardhan190 <vardhankallempudi@gmail.com>
2026-02-20 15:59:36 +0530

    Intial Commit

---
+++ src/Basics/BasicTask/TaskOneCountup.java
@@ -0,0 +1,13 @@
+package Basics.BasicTask;
+
+public class TaskOneCountup {
+    public void countUp() {
+
+        // TODO : countup to 10
+        // using for loop because we know when to stop
+        for (int i = 0; i <= 10; i++) {
+            // now print each element
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Basics.BasicTask.TaskOneCountup;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        TaskOneCountup taskOneCountup = new TaskOneCountup();
+        taskOneCountup.countUp();
+    }
+}
