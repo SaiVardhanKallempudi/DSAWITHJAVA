9ef4762a7c22f106ed12548eb1640098d0279de7
Saivardhan190 <vardhankallempudi@gmail.com>
2026-02-20 23:38:15 +0530

    Day1

---
+++ src/Basics/Patterns/NumberPatterns.java
@@ -0,0 +1,15 @@
+package Basics.Patterns;
+
+public class NumberPatterns {
+    public static void main(String[] args) {
+        printPattern(5);
+    }
+    public static void printPattern(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ src/Basics/Patterns/SquarePattern.java
@@ -0,0 +1,49 @@
+package Basics.Patterns;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+//        square(5);
+//        System.out.println();
+//        rightTriangle(5);
+//        System.out.println();
+//        reverseTriangle(5);
+//        System.out.println();
+        leftTriangle(5);
+    }
+    public static void square(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void rightTriangle(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void reverseTriangle(int n){
+        for (int i = n; i >0; i--) {
+            for (int j = i; j >0 ; j--) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void leftTriangle(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2*(n-i)-1; j++) {
+                System.out.print(" ");
+            }
+                for (int k = 0; k<=i; k++) {
+                    System.out.print("* ");
+                }
+            System.out.println();
+        }
+    }
+}
