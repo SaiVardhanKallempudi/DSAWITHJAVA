1ca451f5ffaaf76162dc6bb083235610d82c3cf0
Saivardhan190 <vardhankallempudi@gmail.com>
2026-02-26 15:00:13 +0530

    Day3

---
+++ src/Day3/ArrayReverse.java
@@ -0,0 +1,23 @@
+package Day3;
+
+import java.util.Arrays;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(reverse(new int[] {1,2,3,4,5,6})));
+    }
+    public static int[] reverse(int[] arr){
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+        for (int i = 0; i < n/2; i++) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return arr;
+    }
+}
---
+++ src/Day3/MaxNumber.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class MaxNumber {
+    public static void main(String[] args) {
+        int[] arr = {-5,-3,-6,-1,-7,-5};
+        System.out.println(max(arr));
+    }
+    public static int max(int[] arr){
+        int max=Integer.MIN_VALUE;
+        for(int num:arr){
+            max = Math.max(num,max);
+        }
+        return max;
+    }
+}
